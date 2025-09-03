24940d3af99f69cd612534de222c353d2b2321a0
Michael <89726435+Michael-YZhou@users.noreply.github.com>
2025-09-03 13:19:08 +1000

    Create the DashboardWrapper component to provide a sidebar + navbar layout for all dashboard pages.

---
+++ client/src/components/DashboardWrapper/DashboardWrapper.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const DashboardWrapper = ({ children }: Props) => {
+  return (
+    <div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
+      {/* sidebar */}
+      sidebar
+      <main className="dark:bg-dark-bg flex w-full flex-col bg-gray-50 md:pl-64">
+        {/* navbar */}
+        navbar
+        {children}
+      </main>
+    </div>
+  );
+};
+
+export default DashboardWrapper;
