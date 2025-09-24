3abd548de0e7a1a33ba716843427de913406cce0
Michael <89726435+Michael-YZhou@users.noreply.github.com>
2025-09-24 16:55:17 +1000

    Create the Board view in the project page and fetch tasks of the specific board from API.

--- client/src/app/projects/BoardView/BoardView.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-type Props = {};
-
-const BoardView = (props: Props) => {
-  return <div>BoardView</div>;
-};
-
-export default BoardView;
--- client/src/app/projects/[id]/page.tsx
+++ client/src/app/projects/[id]/page.tsx
@@ -1,17 +1,24 @@
 "use client";
 
+import { useState } from "react";
 import { useParams } from "next/navigation";
-import React, { useState } from "react";
 import ProjectHeader from "@/app/projects/ProjectHeader/ProjectHeader";
+import Board from "@/app/projects/BoardView/Board";
 
 const Project = () => {
   const { id } = useParams();
   const [activeTab, setActiveTab] = useState<string>("Board");
+  const [isModalNewTaskOpen, setIsModalNewTaskOpen] = useState(false);
 
   return (
     <div>
       <ProjectHeader activeTab={activeTab} setActiveTab={setActiveTab} />
-      {/* {activeTab === "Board" && <Board />} */}
+      {activeTab === "Board" && (
+        <Board
+          id={id as string}
+          setIsModalNewTaskOpen={setIsModalNewTaskOpen}
+        />
+      )}
     </div>
   );
 };
