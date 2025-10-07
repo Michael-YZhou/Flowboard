db5494ee775ec6c84a9ff3569a7a6c5c4d2b2f13
Michael <89726435+Michael-YZhou@users.noreply.github.com>
2025-10-07 15:06:32 +1100

    frontend/feature/create-the-home-page

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,7 +1,9 @@
+import HomePage from "./home/page";
+
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      hi
+    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-sans sm:p-20">
+      <HomePage />
     </div>
   );
 }
