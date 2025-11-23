2dc3b6c0cc48c7df3353c3b37478047c591e0b5d
Michael <89726435+Michael-YZhou@users.noreply.github.com>
2025-11-23 22:12:13 +1100

    fix error in next.config

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,9 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   eslint: {
     ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "flowboard-s3-bucket.s3.ap-southeast-2.amazonaws.com",
+        pathname: "/**",
+      },
+    ],
   },
 };
 
